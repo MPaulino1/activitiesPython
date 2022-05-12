73db2ea77bbc10c4b3fde42f89c50259f3f0454c
Manoel Paulino <mpaulino.me@gmail.com>
2022-05-12 16:22:51 -0300

    ex

---
+++ list3.py
@@ -0,0 +1,14 @@
+
+lista = ['Arroz', 'Feijão', 'Carne','Frango', 'Legumes']
+
+print('Lista antes:', lista)
+
+lista.pop(0)
+
+
+
+print('Lista depois:', lista)
+
+del(lista[0:2])
+
+print('Lista com del:', lista)
